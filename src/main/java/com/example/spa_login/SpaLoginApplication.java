8d3a2886501bf76e715ec45f10a22dd220460c9a
dhson <saozzang@gmail.com>
2025-11-17 22:00:17 +0900

    first commit

---
+++ src/main/java/com/example/spa_login/SpaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.spa_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaLoginApplication.class, args);
+	}
+
+}
